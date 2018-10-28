b3eab7e41021fb7c10f4f63d80a0db74a771842a
m0wer <m0wer@autistici.org>
2018-10-28 23:31:00 +0100

    [feat] Initial commit.

--- README.md
+++ README.md
@@ -2,12 +2,19 @@
 
 Ansible role to setup a **dropbear** SSH server in a **busybox** in the
 **initramfs** so you can be able to connect remotely and enter the **LUKS**
-password of your fully (except for */boot*) encrypted server.
+password of your fully (except for */boot*) encrypted server. If you specify
+a `static_ip`, it will be set up, otherwise the default DHCP option will be
+used.
 
 Based on
 [pbworks](https://www.pbworks.net/ubuntu-guide-dropbear-ssh-server-to-unlock-luks-encrypted-pc/)
 and
 [eugenemdavis](https://www.eugenemdavis.com/set-static-ip-initramfs.html).
+
+**Note**: It only supports RSA SSH keys for the moment.
+
+Uses the *crypt_unlock.sh* from
+[gusennan](https://gist.github.com/gusennan/712d6e81f5cf9489bd9f)
 
 ## Requirements
 
@@ -16,6 +23,15 @@ boot partition and the rest under LUKS encryption that you are able to unlock
 by physically entering the LUKS password to the server.
 
 ## Role Variables
+
+* `ssh_pub_key`: Local path to your public SSH key
+* `static_ip`: Static IP for the **initramfs**.
+* `remote_ip`: Allowed remote IP address to recieve connections from. Empty by
+default (incoming connections aren't restricted to an specific IP address).
+* `gateway`: Gateway IP address.
+* `netmask`: Netmask.
+* `hostname`: Hostname.
+* `network_interface`: Main network interface to setup the static IP on.
 
 ## Dependencies
 
@@ -26,7 +42,6 @@ None.
 ```yaml
 - name: Setup dropbear SSH server in the initramfs
   hosts: all
-  vars:
   roles:
     - role: dropbear_luks
 ```
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,79 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_initramfs_file(host):
+    initramfs_file = host.file('/etc/initramfs-tools/initramfs.conf')
+
+    assert initramfs_file.exists
+    assert initramfs_file.user == 'root'
+    assert initramfs_file.group == 'root'
+    assert initramfs_file.contains('^BUSYBOX=y$')
+    assert initramfs_file.contains('^DROPBEAR=y$')
+    assert initramfs_file.contains(
+            '^IP=172.16.100.12::172.16.100.1:255.255.255.0:test:eth0$')
+
+
+def test_rsa_key_file(host):
+    rsa_key_file = host.file('/etc/dropbear-initramfs/id_rsa')
+
+    assert rsa_key_file.exists
+    assert rsa_key_file.user == 'root'
+    assert rsa_key_file.group == 'root'
+    assert rsa_key_file.contains('-----BEGIN RSA PRIVATE KEY-----')
+    assert rsa_key_file.contains('-----END RSA PRIVATE KEY-----')
+
+
+def test_rsa_pub_key_file(host):
+    rsa_pub_key_file = host.file('/etc/dropbear-initramfs/id_rsa.pub')
+
+    assert rsa_pub_key_file.exists
+    assert rsa_pub_key_file.user == 'root'
+    assert rsa_pub_key_file.group == 'root'
+    assert rsa_pub_key_file.contains('^ssh-rsa *')
+
+
+def test_authorized_keys_file(host):
+    authorized_keys_file = host.file('/etc/dropbear-initramfs/authorized_keys')
+
+    assert authorized_keys_file.exists
+    assert authorized_keys_file.user == 'root'
+    assert authorized_keys_file.group == 'root'
+    assert authorized_keys_file.contains('^ssh-rsa *')
+
+
+def test_dropbear_defaults_file(host):
+    dropbear_defaults_file = host.file('/etc/default/dropbear')
+
+    assert dropbear_defaults_file.exists
+    assert dropbear_defaults_file.user == 'root'
+    assert dropbear_defaults_file.group == 'root'
+    assert dropbear_defaults_file.contains('^NO_START=0*')
+
+
+def test_crypt_unlock_file(host):
+    crypt_unlock_file = host.file('/etc/initramfs-tools/hooks/crypt_unlock.sh')
+
+    assert crypt_unlock_file.exists
+    assert crypt_unlock_file.user == 'root'
+    assert crypt_unlock_file.group == 'root'
+    assert crypt_unlock_file.mode == 0o700
+    assert crypt_unlock_file.contains('#!/bin/sh')
+
+
+def test_dropbear_service(host):
+    dropbear = host.service("dropbear")
+    assert not dropbear.is_enabled
+
+
+def test_grub_defaults_file(host):
+    grub_defaults_file = host.file('/etc/default/grub')
+
+    assert grub_defaults_file.exists
+    assert grub_defaults_file.user == 'root'
+    assert grub_defaults_file.group == 'root'
+    assert grub_defaults_file.contains('^GRUB_CMDLINE_LINUX_DEFAULT="quiet"$')
